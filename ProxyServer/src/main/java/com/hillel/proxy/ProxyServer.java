cbff1bb1581d5c6fc1627808562054f21105950f
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2024-03-24 17:52:07 +0200

    completed hw7 "Service for collecting news"

---
+++ ProxyServer/src/main/java/com/hillel/proxy/ProxyServer.java
@@ -0,0 +1,14 @@
+package com.hillel.proxy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServer.class, args);
+    }
+
+}
